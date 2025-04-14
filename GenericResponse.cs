772fc18bfbeb75fffd0b9a5378ca56d558b74ac7
Rafael Bonotto <rafareisaba@outlook.com>
2025-04-13 23:11:58 -0300

    Adiciona classe generica para response

---
+++ GenericResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WinFormsAppLogin
+{
+    public class GenericResponse<T> 
+    {
+        public bool Success { get; set; }
+        public T? Response { get; set; }
+        public Guid CorrelationId { get; set; }
+    }
+}
